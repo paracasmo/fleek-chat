6fafd9f31c66fdc4c9cc57bd9f1e87765268bf8c
paracasmo <paracasmo@users.noreply.github.com>
2020-07-15 19:17:48 +0200

    start ecmascrip2015+...-ifying stuff

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-var app = require('express')();
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
-var path = require('path');
-var config = require('./config');
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+const path = require('path');
+const config = require('./config');
 
-var tweets = [];
-var filters = config.filters || [];
-var port = config.port || 3000;
+const tweets = [];
+const filters = config.filters.map((i) => i.toLowerCase()) || [];
+const port = config.port || 3000;
 
-var twitter = require('node-tweet-stream'),
+const twitter = require('node-tweet-stream'),
   t = new twitter({
     consumer_key: config.twitter.consumer_key,
     consumer_secret: config.twitter.consumer_secret,
@@ -20,24 +20,16 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-http.listen(config.port, function () {
-  console.log('listening on *:' + port);
-});
+http.listen(config.port, () => console.log('listening on *:' + port));
 
-io.on('connection', function (socket) {
+io.on('connection', socket =>  {
   console.log('user connected');
-  socket.on('disconnect', function () {
-    console.log('user disconnected');
-  });
+  socket.on('disconnect', () => console.log('user disconnected'));
 });
 
-t.on('tweet', function (tweet) {
-  filter(tweet, tweets, filters);
-});
+t.on('tweet', tweet =>filter(tweet, tweets, filters));
 
-t.on('error', function (err) {
-  io.emit("ohnoes");
-});
+t.on('error', err => io.emit('ohnoes'));
 
 t.track(process.argv.slice(2) || 'fleek');
 console.log('Keywords:', t.tracking());
@@ -47,12 +39,12 @@ console.log('Languages:', t.languages());
 
 // filter out tweets containing strings from filters
 function filter(tweet, tweets, filters) {
+  const tweetText = getTetweetText(tweet);
 
-  var tweetText = getTetweetText(tweet);
-
-  var containing = false;
-  for (var i = 0; i < filters.length; i++) {
-    if (contains(tweetText, filters[i])) {
+  let containing = false;
+  for (let i = 0; i < filters.length; i++) {
+    
+    if (tweetText.toLowerCase().includes(filters[i])) {
       containing = true;
       break;
     }
@@ -62,20 +54,16 @@ function filter(tweet, tweets, filters) {
     tweets.push(tweet);
 }
 
-// can't believe ecmascript is only now about to fix this
-function contains(subject, object) {
-  return subject.search(object) !== -1;
-}
-
-var serveTweet = function () {
+const serveTweet = () => {
   if (tweets.length > 0) {
-    var tweet = tweets.splice(Math.floor(Math.random() * tweets.length), 1)[0];
-    io.emit('tweet', tweet.user.screen_name + ": " + getTetweetText(tweet));
+    const tweet = tweets.splice(Math.floor(Math.random() * tweets.length), 1)[0];
+    const tweetText = getTetweetText(tweet);
+    io.emit('tweet', `${tweet.user.screen_name}: ${tweetText}`);
   }
 }
 
-function getTetweetText(tweet) {
-  if(tweet && tweet.truncated) {
+const getTetweetText = tweet => {
+  if (tweet && tweet.truncated) {
     return tweet.extended_tweet.full_text;
   } else {
     return tweet.text;
