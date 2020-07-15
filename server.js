04e5f378c7d750c740549cea35fddc188c4ae84c
paracasmo <paracasmo@users.noreply.github.com>
2020-07-15 19:59:21 +0200

    trying to see what i can get away with 😏

--- server.js
+++ server.js
@@ -1,73 +1,52 @@
-const app = require('express')();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
-const path = require('path');
-const config = require('./config');
+// imports
+const app = require('express')(),
+  http = require('http').createServer(app),
+  io = require('socket.io')(http),
+  path = require('path'),
+  config = require('./config'),
+  twitter = require('node-tweet-stream')
 
-const tweets = [];
-const filters = config.filters.map((i) => i.toLowerCase()) || [];
-const port = config.port || 3000;
+// consts
+const t = new twitter({
+  consumer_key: config.twitter.consumer_key,
+  consumer_secret: config.twitter.consumer_secret,
+  token: config.twitter.token,
+  token_secret: config.twitter.token_secret
+}),
+  tweets = [],
+  filters = config.filters.map((i) => i.toLowerCase()) || [],
+  port = config.port || 3000
 
-const twitter = require('node-tweet-stream'),
-  t = new twitter({
-    consumer_key: config.twitter.consumer_key,
-    consumer_secret: config.twitter.consumer_secret,
-    token: config.twitter.token,
-    token_secret: config.twitter.token_secret
-  });
+// configure http app stuff
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')))
+http.listen(config.port, () => console.log('listening on *:' + port))
+io.on('connection', socket => {
+  console.log('user connected')
+  socket.on('disconnect', () => console.log('user disconnected'))
+})
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
+// configure twitter
+t.track(process.argv.slice(2) || 'fleek')
+t.language(config.languages)
+t.on('tweet', tweet => addTweet(tweet))
+t.on('error', err => io.emit('ohnoes'))
 
-http.listen(config.port, () => console.log('listening on *:' + port));
-
-io.on('connection', socket =>  {
-  console.log('user connected');
-  socket.on('disconnect', () => console.log('user disconnected'));
-});
-
-t.on('tweet', tweet =>filter(tweet, tweets, filters));
-
-t.on('error', err => io.emit('ohnoes'));
-
-t.track(process.argv.slice(2) || 'fleek');
-console.log('Keywords:', t.tracking());
-
-t.language(config.languages);
-console.log('Languages:', t.languages());
-
-// filter out tweets containing strings from filters
-function filter(tweet, tweets, filters) {
-  const tweetText = getTetweetText(tweet);
-
-  let containing = false;
-  for (let i = 0; i < filters.length; i++) {
-    
-    if (tweetText.toLowerCase().includes(filters[i])) {
-      containing = true;
-      break;
-    }
-  }
-
-  if (!containing)
-    tweets.push(tweet);
+const addTweet = tweet => {
+  if (filters.filter(f => !getTetweetText(tweet).includes(f)))
+    tweets.push(tweet)
 }
 
 const serveTweet = () => {
   if (tweets.length > 0) {
-    const tweet = tweets.splice(Math.floor(Math.random() * tweets.length), 1)[0];
-    const tweetText = getTetweetText(tweet);
-    io.emit('tweet', `${tweet.user.screen_name}: ${tweetText}`);
+    const tweet = tweets.splice(Math.floor(Math.random() * tweets.length), 1)[0]
+    const tweetText = getTetweetText(tweet)
+    io.emit('tweet', `${tweet.user.screen_name}: ${tweetText}`)
   }
 }
 
-const getTetweetText = tweet => {
-  if (tweet && tweet.truncated) {
-    return tweet.extended_tweet.full_text;
-  } else {
-    return tweet.text;
-  }
-}
+const getTetweetText = tweet => tweet.truncated ? tweet.extended_tweet.full_text : tweet.text
 
-setInterval(serveTweet, config.tweetDelay);
+// do the thing
+console.log('Keywords:', t.tracking())
+console.log('Languages:', t.languages())
+setInterval(serveTweet, config.tweetDelay)
